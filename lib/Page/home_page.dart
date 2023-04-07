b9e29a688bd30173796ebdc49f6ee7f4d104327a
tuantcybi@gmail.com <tuantcybi@gmail.com>
2023-04-07 11:31:32 +0700

    Initial commit

---
+++ lib/Page/home_page.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+class HomePage  extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+     return Scaffold(
+       body: Center(
+         child:Column(
+           mainAxisAlignment: MainAxisAlignment.spaceAround,
+           children: [
+            const Padding(padding: EdgeInsets.only(top: 80)),
+             const Text("Bluetooth mouse"),
+            const Padding(padding: EdgeInsets.only(top: 40)),
+            SizedBox(
+              width: 200,
+              child: TextField(
+                keyboardType: TextInputType.number ,
+                inputFormatters: [FilteringTextInputFormatter.digitsOnly],
+              ),
+            ),
+             const Padding(padding: EdgeInsets.only(top: 40)),
+             ElevatedButton(
+               onPressed: (){},
+               child: Text('Check'),
+             )
+           ],
+         ),
+       ),
+     );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+import 'Page/home_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Name Your Price',
+      theme: ThemeData(
+
+        primarySwatch: Colors.blue,
+      ),
+      debugShowCheckedModeBanner: false,
+      home: const HomePage(),
+    );
+  }
+}
+
+
